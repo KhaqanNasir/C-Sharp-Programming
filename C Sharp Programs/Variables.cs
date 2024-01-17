9874a5cda2e1006f5b89e3b9d2cca436af6781cd
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-17 21:50:09 +0500

    Add Variables Class

---
+++ C Sharp Programs/Variables.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace C_Sharp_Programs
+{
+     class Variables
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("..........VARIABLES..........");
+            int x = 39;   // x is the variable and it is name in the memory
+            Console.WriteLine(x);
+            Console.Read();
+        }
+    }
+}
