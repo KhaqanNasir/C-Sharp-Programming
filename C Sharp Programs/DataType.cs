74536afd270581cc8ad1ba2320164e2739e20164
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-17 22:00:32 +0500

    Add Data Type Class

---
+++ C Sharp Programs/DataType.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace C_Sharp_Programs
+{
+     class DataType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("..........Data Types..........");
+
+            int x = 39;                 // int is the data type and it can store whole numbers
+            Console.WriteLine(x);
+
+            double y = 3.39503;        // double is the data type and it can store value of points greater than 7
+            Console.WriteLine(y);
+
+            float z = 3.85f;           // float is the data type and it can store points values
+            Console.WriteLine(z);
+
+            char ch = '@';            // char is the data type and it can store value pf single character
+            Console.WriteLine(ch);
+
+            string s = "Khaqan Nasir";// string is the data type and it can write in double quotes, used for print a message
+            Console.WriteLine(s);
+
+            bool b = true;           // Bool is the data type used for true or false
+            Console.WriteLine(b);
+
+            decimal d = 3.593945030m;// Decimal is the data type used for long values
+            Console.WriteLine(d);
+
+            Console.Read();         // Console.Read is used for hold the screen
+        }
+    }
+}
