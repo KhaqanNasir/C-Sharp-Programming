751addd0610cec49c46fdf7d382104a3e1398a2c
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-17 21:46:28 +0500

    Add Project Folder and Hello World Program

---
+++ C Sharp Programs/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace C_Sharp_Programs
+{
+     class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!...");
+            Console.Read();
+        }
+    }
+}
