2a7aef77390a547a88e60eefdb5e10f510025cf4
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-17 22:15:03 +0500

    Add Operators Class

---
+++ C Sharp Programs/Operators.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace C_Sharp_Programs
+{
+     class Operators
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("......Operators.......");
+
+            int x = 5;
+            int y = 7;
+
+            Console.WriteLine(x + y); // + is the Arithmetic Operator to Add two numbers
+            Console.WriteLine(x - y); // - is the Arithmetic Operator to Subtract two numbers
+            Console.WriteLine(x * y); // * is the Arithmetic Operator to Multiply two numbers
+            Console.WriteLine(x / y); // / is the Arithmetic Operator to Divide two numbers
+            Console.WriteLine(x % y); // % is the Arithmetic Operator to get the remainder of two numbers
+
+            Console.WriteLine(x < y); // < is the Relational Operator to check less than between two numbers return true of false
+            Console.WriteLine(x <= y); // <= is the Relational Operator to check less than and equal to between two numbers return true of false 
+            Console.WriteLine(x > y); // > is the Relational Operator to check greater than between two numbers return true of false
+            Console.WriteLine(x >= y); // >= is the Relational Operator to check greater than and equal to between two numbers return true of false
+            Console.WriteLine(x == y); // == is the Relational Operator to check equal to between two numbers return true of false
+            Console.WriteLine(x != y); // != is the Relational Operator to check not equal to between two numbers return true of false
+
+            Console.WriteLine(x++);  // ++ is the Unary Operators to increment 1 in the x
+            Console.WriteLine(x--);  // -- is the Unary Operators to decrement 1 in the x
+
+            x = 100;  // = is the Assignment Operator to Assign Values
+            Console.WriteLine(x);
+
+            Console.Read();         // Console.Read is used for hold the screen
+        }
+    }
+}
