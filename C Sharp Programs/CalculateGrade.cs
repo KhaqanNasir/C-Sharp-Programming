f149c0950b8595ed2e51d7a2ada9c0abe5a95ef4
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-18 15:03:45 +0500

    Calculate Grade Using If Else

---
+++ C Sharp Programs/CalculateGrade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C_Sharp_Programs
+{
+     class CalculateGrade
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Numbers");
+            double num = Convert.ToDouble(Console.ReadLine());  
+            if(num >= 90)
+            {
+                Console.WriteLine("A Grade");
+            } else if(num >= 80)
+            {
+                Console.WriteLine("B Grade");
+            } else if (num >= 70)
+            {
+                Console.WriteLine("C Grade");
+            } else if(num >= 60)
+            {
+                Console.WriteLine("D Grade");
+            } else if (num >= 50)
+            {
+                Console.WriteLine("E Grade");
+            }
+            else
+            {
+                Console.WriteLine("F Grade");
+            }
+
+            Console.Read();
+        }
+    }
+}
