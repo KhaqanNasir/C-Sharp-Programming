7583a608a3a8c69c9394d32153587e81939df7ca
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-18 15:19:32 +0500

    Add Switch Condition Class

---
+++ C Sharp Programs/Switch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace C_Sharp_Programs
+{
+     class Switch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Numbers 0-5");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Number is "+num);
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Number is "+num);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Number is " + num);
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Number is " + num);
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Number is " + num);
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Number is " + num);
+                        break;
+                            }
+                default:
+                    {
+                        Console.WriteLine("Invalid Input");
+                        break;
+                    }
+
+            }
+
+
+            Console.Read();
+        }
+    }
+}
