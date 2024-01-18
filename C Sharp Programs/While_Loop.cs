3d649c015418d390c7220a6e79fdff56e011fdc0
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-18 15:57:06 +0500

    Add While Loop Class\

---
+++ C Sharp Programs/While_Loop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C_Sharp_Programs
+{
+     class While_Loop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....Simple While Loop Example....\n");
+            Console.WriteLine("Enter A Number to print a table");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i <= 10)
+            {
+                Console.WriteLine(num + " x " + i + " = " + num * i);
+                i++;
+            }
+
+            Console.WriteLine("\n....Simple Nested While Loop Example....\n");
+            int k = 1;
+            while (k <= 3)
+            {
+                int j = 1;
+                while (j <= 3)
+                {
+                    Console.WriteLine(k + " " + j);
+                    j++;
+                }
+                k++;
+            }
+
+
+            Console.Read();
+        }
+    }
+}
