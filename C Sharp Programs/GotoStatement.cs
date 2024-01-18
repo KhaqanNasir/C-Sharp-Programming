fdce81096d7b8fa2ee3d97781999a10d3baa4ca7
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-18 16:41:33 +0500

    Add Goto Statement Class

---
+++ C Sharp Programs/GotoStatement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_Sharp_Programs
+{
+     class GotoStatement
+    {
+        static void Main(string[] args)
+        {   
+            again:
+            Console.WriteLine("Enter your age");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 18)
+            {
+                Console.WriteLine("Eligible for vote");
+            }
+            else
+            {
+                Console.WriteLine("Not Eligible for vote");
+                goto again;
+            }
+
+
+            Console.Read();
+        }
+    }
+}
