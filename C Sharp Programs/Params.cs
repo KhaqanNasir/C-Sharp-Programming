09a9f272d532d69fcba24277aa419efb93613258
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-19 16:22:46 +0500

    Add Params Keyword Class

---
+++ C Sharp Programs/Params.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_Sharp_Programs
+{
+     class Params
+    {
+        public void display(params int[] array)     // just only write the params keyword
+        {
+            for(int i =0;i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public void display(params object[] array) {   // obect type
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Params p1 = new Params();
+            p1.display(1, 2, 3, 4, 5, 6, 7);    // pass infinity arguments
+            Params p2 = new Params();
+            p2.display("M.Khaqa Nasir","FA22-BCS-039",3.85);   //pass infinite arguments
+           
+
+
+            Console.Read();   // for hold the screen
+        }
+    }
+}
