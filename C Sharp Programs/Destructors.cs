dcca7ee58934199e4c3eb8a814a30a3594f0874c
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-20 22:23:50 +0500

    Destructor

---
+++ C Sharp Programs/Destructors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Destructor
+{
+    int a;
+    int b;
+
+    public Destructor(int a, int b)
+    { 
+        this.a = a;
+        this.b = b;
+    }
+    ~Destructor()   // Destructor of the class it can destroy the instance of he class
+    {
+        Console.WriteLine("Destroyed");
+    }
+    public void display()
+    {
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+    static void Main(string[] args)
+    {
+        Destructor d1 = new Destructor(1, 2);
+        d1.display();
+
+
+        Console.Read();   // for hold the screen
+
+
+    }
+}
