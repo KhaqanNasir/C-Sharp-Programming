0ebcac2358ac845dbc9ab9a658e3ab611cc68e8c
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-21 15:50:45 +0500

    Add Static Class

---
+++ C Sharp Programs/StaticClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_Sharp_Programs { 
+static class StaticClass   // static class
+{
+        static float piValue = 3.14f;    // static data member
+        static int cube(int n)   // static member function
+        {
+            return n * n * n;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PiValue is " + StaticClass.piValue);
+            Console.WriteLine("Cube of 3 is "+StaticClass.cube(3));
+
+            Console.WriteLine("PiValue is " + StaticClass.piValue);
+            Console.WriteLine("Cube of 5 is " + StaticClass.cube(5));
+
+
+
+            Console.Read();   // for hold the screen
+        }
+
+    }
+}
