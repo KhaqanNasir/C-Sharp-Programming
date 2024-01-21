c3193b95eb9c0b7fcb4dd8ef83a4b7855b96566a
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-21 15:56:01 +0500

    Add Static Constructor Class

---
+++ C Sharp Programs/StaticConstructor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace C_Sharp_Programs { 
+ class StaticConstructor   
+{
+        int id;
+        string name;
+        static string companyName;
+        static StaticConstructor()   // static constructor only initialize the static member
+        {
+            companyName = "Khaqan Istitute";
+        }
+        StaticConstructor(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        void display()
+        {
+            Console.WriteLine(companyName);
+            Console.WriteLine(this.id);
+            Console.WriteLine(this.name);
+        }
+        static void Main(string[] args)
+        {
+            StaticConstructor s1 = new StaticConstructor(11, "Ali");
+            s1.display();
+
+            StaticConstructor s2 = new StaticConstructor(12, "Abdullah");
+            s2.display();
+            
+
+
+
+            Console.Read();   // for hold the screen
+        }
+
+    }
+}
