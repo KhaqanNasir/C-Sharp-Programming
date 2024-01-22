42a66584070f0fc2bdf30b49e0f86ff320d842c0
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-22 16:28:05 +0500

    HAS A Relationship Example

--- C Sharp Programs/MainClass.cs
+++ C Sharp Programs/MainClass.cs
@@ -10,32 +10,41 @@ namespace C_Sharp_Programs
    public class student : University {   // second Class Child / Derived Class
         public int id ;
         public string name ;
-     
-        
+
+        public student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }   
+       
     }
-       public class Topper : student {    // third Class Child / Derived Class
+       public class Topper {   
 
         public float gpa;
         public string department ;
+        public student std;   // use student class as a data member this is called HAS A Relationship
+
+        public Topper(float gpa, string department, student std)
+        {
+            this.gpa = gpa;
+            this.department = department;
+            this.std = std;
+        }
+        public void display()
+        {
+            Console.WriteLine(this.gpa + " " + this.department+" "+this.std.name+" "+this.std.id);
+            Console.WriteLine(student.uniName);
+        }
     }
     class MainClass
         {
             static void Main(string[] args)
             {
-            Console.WriteLine("Show the EXAMPLE of MULTI-LEVEL INHERITENCE\n");
-            
-             Topper t1 = new Topper();
+            Console.WriteLine("\tEXAMPLE of HAS A Relationship\n"); 
 
-            t1.id = 39;
-            t1.name = "M.Khaqan Nasir";
-            t1.department = "Computer Science";
-            t1.gpa = 3.96f;
-
-            Console.WriteLine(t1.id);
-            Console.WriteLine(t1.name);
-            Console.WriteLine(t1.department);
-            Console.WriteLine(t1.gpa);
-            Console.WriteLine(Topper.uniName);
+            student s1 = new student(39, "M.Khaqan Nasir");
+             Topper t1 = new Topper(3.96f,"Computer Science",s1);
+            t1.display();
 
 
                 Console.Read();   // for hold the screen
