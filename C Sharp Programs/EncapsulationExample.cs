4f7b10a8da53af5fc7fe9e23b86ccaea57868736
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-29 22:02:43 +0500

    Encapsulation Example

---
+++ C Sharp Programs/EncapsulationExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace A
+{
+    class zain
+    {
+        public string name {
+            get; set;
+        }
+        public int age { get; set; }
+    }
+
+
+        class EncapsulationExample
+        {
+            static void Main(string[] args)
+            {
+             zain z1 = new zain();
+            z1.name = "M.Khaqan Nasir";
+            z1.age = 20;
+
+            Console.WriteLine("Name : "+z1.name);
+            Console.WriteLine("Age : "+z1.age);
+              
+
+
+                Console.Read();   // for hold the screen
+            }
+
+        }
+
+    }
