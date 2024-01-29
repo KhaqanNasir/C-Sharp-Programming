daea6b588a53cb4422e08c1987067adff49679e1
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-01-29 21:58:42 +0500

    Namespace Example

---
+++ C Sharp Programs/NamespaceExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using A;
+using B;
+
+namespace A
+{
+    class zain
+    {
+        public void show()
+        {
+            Console.WriteLine("Zain Class");
+        }
+    }
+}
+namespace B
+{
+    class ali
+    {
+        public void show()
+        {
+            Console.WriteLine("Ali Class");
+        }
+    }
+}   
+        class NamespaceExample
+        {
+            static void Main(string[] args)
+            {
+              
+        Console.WriteLine("Two Namespace Example");
+        zain z1 = new zain();
+        z1.show();
+        ali a1 = new ali();
+        a1.show();
+              
+
+
+                Console.Read();   // for hold the screen
+            }
+
+        }
+
+    
