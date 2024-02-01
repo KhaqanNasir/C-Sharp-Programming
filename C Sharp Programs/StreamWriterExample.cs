03fc9acfa82a55cb5105588ef05678562f6cb313
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-01 16:03:39 +0500

    Stream Reader and Writer

---
+++ C Sharp Programs/StreamWriterExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace A
+{ 
+   
+        class StreamWriterExample
+        {
+        
+        static void Main(string[] args)
+        {
+            string path = @"D:\streamFile.txt";
+            FileStream file = new FileStream(path,FileMode.OpenOrCreate);
+
+            StreamWriter sr = new StreamWriter(file);
+            StreamReader s = new StreamReader(file);
+
+            sr.WriteLine("Hello i am a C# developer");
+            string line = s.ReadLine();
+            Console.WriteLine(line);
+
+
+            s.Close();
+            sr.Close();
+            file.Close();
+            Console.WriteLine("Successful ");
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
