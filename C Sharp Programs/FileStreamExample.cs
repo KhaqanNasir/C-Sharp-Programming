064017d3f80d71ef7718e62c3d0033af1e1654cf
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-01 15:45:23 +0500

    File Stream Example

---
+++ C Sharp Programs/FileStreamExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace A
+{ 
+   
+        class FileStreamExample
+        {
+        
+        static void Main(string[] args)
+        {
+            string path = @"D:\streamFile.txt";
+            FileStream file = new FileStream(path,FileMode.OpenOrCreate);
+            for(int i = 65; i <=90; i++)
+            {
+                file.WriteByte((byte)i);
+            }
+
+            int j=0;
+            while((j=file.ReadByte()) != -1)
+            {
+                Console.WriteLine((char)j);
+            }
+            file.Close();
+            
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
