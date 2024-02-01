828335a5241c0f13d641deac9043f31b2c149ffa
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-01 22:12:40 +0500

    Text Reader and Writer

---
+++ C Sharp Programs/TextWriterExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace A
+{ 
+   
+        class TextWriterExample
+        {
+        
+        static void Main(string[] args)
+        {
+            string path = @"D:\streamFile.txt";
+           TextWriter writer = File.CreateText(path);
+            writer.WriteLine("Khaqan ");
+            writer.WriteLine("Nasir");
+            writer.Close();
+
+            TextReader reader = File.OpenText(path);
+            Console.WriteLine(reader.ReadToEnd());  
+            reader.Close();
+
+            Console.WriteLine("Successful ");
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
