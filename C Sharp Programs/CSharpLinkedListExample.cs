cbc60f43f1773953cbb6bb43641388c8820e7c9c
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-02 21:30:04 +0500

    Generic LinkedList Class

---
+++ C Sharp Programs/CSharpLinkedListExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace A
+{ 
+   
+        class CSharpLinkedListExample
+        {
+        
+        static void Main(string[] args)
+        {
+            var names = new LinkedList<string>();
+
+            names.AddLast("M.Khaqan Nasir");
+            names.AddLast("Abdullah Rafiq");
+            names.AddLast("Malik Ali Raza Yar");
+            names.AddFirst("Areeb Akram");
+            
+            foreach (var name in names) { Console.WriteLine(name); }
+
+            LinkedListNode<string> node = names.Find("Abdullah Rafiq");
+
+            names.AddAfter(node, "Faizan Mustafa");
+            names.AddBefore(node, "Ahsan Abdullah");
+
+            foreach(var name in names) { Console.WriteLine(name); }
+
+
+
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
