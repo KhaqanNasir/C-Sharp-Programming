3eed0d04c688b27ed2e3c70fcfcf0a16a6d5b3e0
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-02 21:49:33 +0500

    Generic Dictionary Class

---
+++ C Sharp Programs/CSharpDictionaryExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace A
+{ 
+   
+        class CSharpDictionaryExample
+        {
+        
+        static void Main(string[] args)
+        {
+           Dictionary<int,string> dictionary = new Dictionary<int,string>();
+
+            dictionary.Add(1, "M.Khaqan Nasir");
+            dictionary.Add(2, "Abdullah Rafiq");
+            dictionary.Add(3, "Malik Ali Raza Yar");
+            dictionary.Add(4, "Faizan Mustafa");
+            dictionary.Add(5, "Ahsan Abdullah");
+            dictionary.Add(6, "Areeb Akram");
+
+            foreach (var item in dictionary) { Console.WriteLine(item.Key+" "+item.Value); }
+
+            dictionary.Remove(4);
+
+            foreach(var item in dictionary) { Console.WriteLine(item.Key+" "+item.Value); }
+
+           Console.WriteLine(dictionary.TryGetValue(4, out string value));
+
+            dictionary.Clear();
+
+            foreach(var item in dictionary) { Console.WriteLine(item); }
+
+
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
