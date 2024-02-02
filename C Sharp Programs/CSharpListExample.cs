0f37009ea8591fffd0178ceec831c0e3ad0360a0
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-02 15:51:11 +0500

    Generic List Class

---
+++ C Sharp Programs/CSharpListExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace A
+{ 
+   
+        class CSharpListExample
+        {
+        
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            names.Add("M.Khaqan Nasir");
+            names.Add("Abdullah Rafiq");
+            names.Add("Ahsan ");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
