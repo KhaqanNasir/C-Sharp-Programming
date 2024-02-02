1c51e71ed7be48a1dadcb00487c0def4b2e90fee
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-02-02 16:16:50 +0500

    Generic SortedSet Class

---
+++ C Sharp Programs/CSharpSortedSetExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace A
+{ 
+   
+        class CSharpSortedSetExample
+        {
+        
+        static void Main(string[] args)
+        {
+            var names = new SortedSet<string>();
+
+            names.Add("M.Khaqan Nasir");
+            names.Add("Abdullah Rafiq");
+            names.Add("Ahsan ");
+            names.Add("Areeb Akram");
+            names.Add("Faizan Mustafa");
+            names.Add("Malik Ali Raza yar");
+
+            foreach(var a in names) { Console.WriteLine(a); }
+
+            names.Remove("Ahsan ");   // remove one elemnt only
+ 
+            foreach(var a in names) { Console.WriteLine(a); }
+
+
+            names.Clear();  // remove all elements from SortedSet class
+
+            foreach (var name in names) { Console.WriteLine(name); }
+            
+            
+            Console.Read();   // for hold the screen
+            }
+           
+
+        }
+
+    }
